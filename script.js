75a9568150fa336fe85430dc7cc7e808a8da33a9
faisalbazid <91886338+faisalbazid@users.noreply.github.com>
2021-12-03 09:06:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 var randomnumber = Math.floor(Math.random()*100+1);
-
 var restcounter = document.getElementById('restcounter');
 var displaytext = document.getElementById("displaytext");
 var smalbig = document.getElementById("smalbig");
-
 var submit = document.getElementById("submit");
 var input = document.getElementById("guessNum");
  
 var totalcount = 9;
 var countplus = 0;
-
 
 document.getElementById("submit").onclick = (FUNCTION) => {
     var usercheck = Number(guessNum.value);
@@ -17,18 +14,15 @@ document.getElementById("submit").onclick = (FUNCTION) => {
     countplus++;
     input='';
     
-
     if ( totalcount == 10 || totalcount >= 0 ) {
         restcounter.textContent = `${totalcount--}`;
     }
-
 
     if ( usercheck == randomnumber && countplus <= 2  ){
         displaytext.textContent = "Bravo, vous etes un Genie !!!";
         displaytext.style.color = "green";
         smalbig.textContent = '';
     }
-
 
     else if ( usercheck === randomnumber && countplus >= 2 ) {
         displaytext.textContent = 'Félicitations, vous avez gagné après ' +`${countplus}`+ ' tentatives' ;
@@ -39,8 +33,6 @@ document.getElementById("submit").onclick = (FUNCTION) => {
         displaytext.textContent = " YOU LOSS ";
         displaytext.style.color = "#ff0000";
         smalbig.textContent='';
-        
-        
     }
     
     else{
